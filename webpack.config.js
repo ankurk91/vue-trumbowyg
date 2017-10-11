766ae4ccfcc6b881a58d84b7520f7f0a40259787
ankurk91 <pro.ankurk1@gmail.com>
2017-10-11 15:11:39 +0530

    Generate two dist files

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
 module.exports = {
   context: __dirname,
@@ -46,6 +47,7 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['./dist']),
+    new UnminifiedWebpackPlugin()
   ],
   devtool: false,
   performance: {
