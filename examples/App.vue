aaf6d393e51baae30d1be4956e7aa215fe1e55e1
ankurk91 <pro.ankurk1@gmail.com>
2018-10-22 18:28:49 +0530

    Update docs

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark shadow-sm">
+    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark shadow-sm mb-3">
       <span class="navbar-brand mb-0">Vue.js Trumbowyg</span>
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
@@ -9,9 +9,9 @@
       </ul>
     </nav>
 
-    <main class="container my-3">
+    <main class="container">
       <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-3">
 
           <div class="card mb-3">
             <div class="card-body">
@@ -53,7 +53,7 @@
           </form>
         </div>
 
-        <aside class="col-md-4">
+        <aside class="col-md-4 mb-3">
           <div class="card">
             <div class="card-header">
               Links
@@ -71,7 +71,7 @@
         </aside>
       </div>
     </main>
-    <footer class="text-center text-muted small mb-3 mt-1">
+    <footer class="text-center text-muted small mb-3">
       Created by <a href="https://twitter.com/ankurk91" target="_blank" rel="noopener">@ankurk91</a>
     </footer>
   </div>
